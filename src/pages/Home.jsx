3d94c25bf9852c05f0a392f9b9e340d4a3e6c2db
Ian Kamau <78727737+Kamau-ke@users.noreply.github.com>
2025-05-13 23:23:42 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ function Home() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
                     </svg>
                   </Button>
-                    <Link to='projects'>
+                    <Link to='/projects'>
                     <Button>
                     View My Work
                   </Button>
